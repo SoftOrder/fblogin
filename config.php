9c0b6df43418d195e15aeb98f249e5fd0e932ed7
SoftOrder <softorder.developers@outlook.com>
2018-06-19 16:48:28 +0200

    make it better

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+	require_once = "Facebook/autoload.php";
+	$FB = new \Facebook\Facebook([
+		"app_id" => '2060672907553566',
+		"app_secret" => 'd56cca37fa6d0c2349e94a631cd8471d',
+		"default_graph_version" => 'v2.10'
+	]);
+
+
+
+?>
